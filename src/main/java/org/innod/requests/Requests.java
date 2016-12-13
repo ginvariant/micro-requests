9d9ee8d36f3bc99f9ab9666c390a1fe4ad15ba91
Amador Molina <amador.molina.soberino@gmail.com>
2016-12-13 08:11:07 +0100

    {no-track} : Remove cluster manager.

--- src/main/java/org/innod/requests/Requests.java
+++ src/main/java/org/innod/requests/Requests.java
@@ -1,17 +1,14 @@
 package org.innod.requests;
 
-import org.innod.requests.verticles.RequestsVerticle;
-
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
-import io.vertx.core.spi.cluster.ClusterManager;
-import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
+
+import org.innod.requests.verticles.RequestsVerticle;
 
 public class Requests {
 
 	public static void main(String[] args) {
-		ClusterManager mgr = new HazelcastClusterManager();
-		VertxOptions options = new VertxOptions().setClusterManager(mgr).setClustered(true);
+		VertxOptions options = new VertxOptions().setClustered(true);
 		Vertx.clusteredVertx(options, res -> {
 			if (res.succeeded()) {
 				Vertx vertx = res.result();
